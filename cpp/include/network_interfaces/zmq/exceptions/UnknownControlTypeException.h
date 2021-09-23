f7d637886116ade73860c729dd8fb8a43e230b3b
Dominic Reber <dominic.reber@epfl.ch>
2021-09-23 15:06:32 +0200

    Add docstrings

--- cpp/include/network_interfaces/zmq/exceptions/UnknownControlTypeException.h
+++ cpp/include/network_interfaces/zmq/exceptions/UnknownControlTypeException.h
@@ -3,6 +3,11 @@
 #include <exception>
 
 namespace network_interfaces::zmq::exceptions {
+
+/*
+ * @class UnknownControlTypeException
+ * @brief Exception that is thrown when a control_type of a CommandMessage is unknown
+ */
 class UnknownControlTypeException : public std::runtime_error {
 public:
   explicit UnknownControlTypeException(const std::string& msg) : runtime_error(msg) {};
