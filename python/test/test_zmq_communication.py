dc6fe50681d06024c9d22dd547b5e2a3e8a37a0e
domire8 <71256590+domire8@users.noreply.github.com>
2022-06-21 10:12:47 +0200

    Make Python tests pass more reliably with Executor (#18)

--- python/test/test_zmq_communication.py
+++ python/test/test_zmq_communication.py
@@ -1,6 +1,6 @@
+import concurrent.futures
 import numpy as np
 import state_representation as sr
-import threading
 import time
 import unittest
 import zmq
@@ -39,7 +39,8 @@ class TestZMQNetworkInterface(unittest.TestCase):
 
         state = network.StateMessage(self.robot_state, self.robot_joint_state, self.robot_jacobian, self.robot_mass)
         command = []
-        for i in range(200):
+        start_time = time.time()
+        while time.time() - start_time < 2.:
             network.send_state(state, state_publisher)
             command = network.receive_command(command_subscriber)
             time.sleep(0.01)
@@ -53,7 +54,8 @@ class TestZMQNetworkInterface(unittest.TestCase):
 
         command = network.CommandMessage(self.control_type, self.control_command)
         state = []
-        for i in range(200):
+        start_time = time.time()
+        while time.time() - start_time < 2.:
             network.send_command(command, command_publisher)
             state = network.receive_state(state_subscriber)
             time.sleep(0.01)
@@ -62,12 +64,9 @@ class TestZMQNetworkInterface(unittest.TestCase):
         self.received_state = state
 
     def test_communication(self):
-        robot = threading.Thread(target=self.robot)
-        control = threading.Thread(target=self.control)
-        robot.start()
-        control.start()
-        robot.join()
-        control.join()
+        with concurrent.futures.ThreadPoolExecutor(max_workers=2) as e:
+            e.submit(self.robot)
+            e.submit(self.control)
 
         [self.assertEqual(self.received_command.control_type[i], self.control_type[i]) for i in
          range(len(self.control_type))]
