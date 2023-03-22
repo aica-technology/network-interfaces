1156ddc90c09456f56ad4390a9c9e65551b84aeb
Dominic Reber <71256590+domire8@users.noreply.github.com>
2023-03-22 14:48:05 +0100

    Use control libraries version 7.0 (#21)

--- python/setup.py
+++ python/setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     url="https://github.com/aica-technology/network-interfaces",
     packages=setuptools.find_packages(),
     install_requires=[
-        "control-libraries>=6.0.0",
+        "control-libraries>=7.0.0",
         "pyzmq>=22.3"
     ],
     classifiers=[
