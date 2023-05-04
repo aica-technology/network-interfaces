279dd93aec85e6b98f7028d9a1b7e4dd9035f758
Dominic Reber <dominic@aica.tech>
2023-05-04 17:43:41 +0200

    Add pkg-config files

--- source/install.sh
+++ source/install.sh
@@ -12,22 +12,26 @@ Options:
                            automatically approve install steps.
   -d, --dir [path]         Configure the installation directory
                            (default: ${INSTALL_DESTINATION}).
+  --build-tests            Build the unittest targets.
+  --clean                  Delete any previously installed header
+                           files from ${INSTALL_DESTINATION}/include and any
+                           shared library files from ${INSTALL_DESTINATION}/lib.
+  --cleandir [path]        Delete any previously installed header
+                           and library files from the specified path.
   -h, --help               Show this help message."
 
 function install_cppzmq() {
-  if [ "${INSTALL_CPP}" == 1 ]; then
-    apt-get update && apt-get install "${AUTO_INSTALL}" libzmq3-dev || exit 1
+  apt-get update && apt-get install "${AUTO_INSTALL}" libzmq3-dev || exit 1
 
-    mkdir -p "${SCRIPT_DIR}"/install
-    cd "${SCRIPT_DIR}"/install || exit 1
-    wget https://github.com/zeromq/cppzmq/archive/v${CPPZMQ_VERSION}.tar.gz -O cppzmq-${CPPZMQ_VERSION}.tar.gz &&
-      tar -xzf cppzmq-${CPPZMQ_VERSION}.tar.gz &&
-      rm cppzmq-${CPPZMQ_VERSION}.tar.gz
+  mkdir -p "${SCRIPT_DIR}"/install
+  cd "${SCRIPT_DIR}"/install || exit 1
+  wget https://github.com/zeromq/cppzmq/archive/v${CPPZMQ_VERSION}.tar.gz -O cppzmq-${CPPZMQ_VERSION}.tar.gz &&
+    tar -xzf cppzmq-${CPPZMQ_VERSION}.tar.gz &&
+    rm cppzmq-${CPPZMQ_VERSION}.tar.gz
 
-    cd "${SCRIPT_DIR}"/install/cppzmq-"${CPPZMQ_VERSION}" || exit 1
-    mkdir build && cd build && cmake .. -DCPPZMQ_BUILD_TESTS=OFF && make -j install || exit 1
-    ldconfig
-  fi
+  cd "${SCRIPT_DIR}"/install/cppzmq-"${CPPZMQ_VERSION}" || exit 1
+  mkdir build && cd build && cmake .. -DCPPZMQ_BUILD_TESTS=OFF && make -j install || exit 1
+  ldconfig
 
   cd "${SCRIPT_DIR}" && rm -rf "${SCRIPT_DIR}"/install || exit 1
 }
@@ -42,10 +46,24 @@ install_communication_interfaces () {
   cd "${SCRIPT_DIR}" || exit 1
 }
 
+function uninstall() {
+  function delete_components() {
+    rm -r "${INSTALL_DESTINATION}"/include/communication_interfaces
+    rm -r "${INSTALL_DESTINATION}"/lib/libcommunication_interfaces*.so
+  }
+
+  delete_components >/dev/null 2>&1
+
+  echo "Deleted any communication_interfaces artefacts from ${INSTALL_DESTINATION}."
+}
+
 while [ "$#" -gt 0 ]; do
   case "$1" in
   -y|--auto) AUTO_INSTALL="-y"; shift 1;;
   -d|--dir) INSTALL_DESTINATION=$2; shift 2;;
+  --build-tests) BUILD_TESTING="ON"; shift 1;;
+  --clean) uninstall; exit 0;;
+  --cleandir) INSTALL_DESTINATION=$2; uninstall; exit 0;;
   -h|--help) echo "${HELP_MESSAGE}"; exit 0;;
   -*) echo "Unknown option: $1" >&2
       echo "${FAIL_MESSAGE}"
@@ -57,6 +75,13 @@ $(pkg-config --exists state_representation) || exit 1
 if [ "$?" == 1 ]; then
   echo ">>> STATE REPRESENTATION LIBRARY NOT FOUND!"
   echo ">>> Install state_representation from https://github.com/aica-technology/control-libraries/source"
+  exit 1
+fi
+
+$(pkg-config --exists clproto) || exit 1
+if [ "$?" == 1 ]; then
+  echo ">>> CL PROTO LIBRARY NOT FOUND!"
+  echo ">>> Install clproto from https://github.com/aica-technology/control-libraries/protocol"
   exit 1
 fi
 
