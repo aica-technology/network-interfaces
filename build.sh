68f71b42804b048e3a8520f6ef0156583965c0f7
Dominic Reber <71256590+domire8@users.noreply.github.com>
2023-09-18 17:40:42 +0200

    build: rename Dockerfile and update helper scripts (#58)
    Rename Dockerfile and remove deprecated scripts and
    CMakeLists.
    
    Update READMEs.

---
+++ build.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+IMAGE_NAME=ghcr.io/aica-technology/communication-interfaces
+IMAGE_TAG=latest
+
+ROS2_VERSION=humble
+
+HELP_MESSAGE="Usage: build.sh [options]
+Options:
+  --test                      Target the test layer to run the tests.
+
+  --ros2-version <VERSION>    Specify the version of ROS 2 to use.
+                              (default: $ROS2_VERSION)
+
+  -v|--verbose             Set the build output to verbose.
+
+  --cache-id <id>          Invalidate the mount cache (e.g. CMake build folder)
+                           by providing a new value.
+
+  -r|--no-cache            Invalidate all cache (layer + mount).
+
+  -h|--help                Show this help message.
+"
+
+TEST=0
+BUILD_FLAGS=()
+while [ "$#" -gt 0 ]; do
+  case "$1" in
+    --test) BUILD_FLAGS+=(--target=test); TEST=1; IMAGE_TAG=test; shift 1;;
+    --ros2-version) ROS2_VERSION=$2; shift 2;;
+    -v|--verbose) BUILD_FLAGS+=(--progress=plain); shift 1;;
+    --cache-id) BUILD_FLAGS+=(--build-arg CACHEID=$2); shift 2;;
+    -r|--no-cache) BUILD_FLAGS+=(--no-cache); BUILD_FLAGS+=(--build-arg CACHEID=$(date +%s)); shift 1;;
+    -h|--help) echo "$HELP_MESSAGE"; exit 0;;
+    -*) echo "Unknown option: $1" >&2; echo "$HELP_MESSAGE"; exit 1;;
+  esac
+done
+
+BUILD_FLAGS+=(--build-arg ROS2_VERSION="${ROS2_VERSION}")
+BUILD_FLAGS+=(-t "${IMAGE_NAME}:${IMAGE_TAG}")
+
+DOCKER_BUILDKIT=1 docker build "${BUILD_FLAGS[@]}" . || exit 1
